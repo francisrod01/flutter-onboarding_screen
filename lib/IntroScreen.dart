b5dd164ead907cf9663f9494ca5266f32e0f0986
Francis Rodrigues <francis.rod01@outlook.com>
2022-07-26 23:19:24 -0300

    feat(main screen): Added the control buttons

--- lib/IntroScreen.dart
+++ lib/IntroScreen.dart
@@ -10,19 +10,55 @@ class IntroScreen extends StatelessWidget {
         scrollPhysics: BouncingScrollPhysics(),
         pages: [
           PageViewModel(
-              titleWidget: Text("Write Title of Page",
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                  )),
-              body:
-                  "Write the more description of the page. Write more and more description for this page.",
-              image: Image.asset(
-                "images/1.png",
-                height: 400,
-                width: 400,
-              )),
+            titleWidget: Text(
+              "Write Title of Page",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            body:
+                "Write the more description of the page. Write more and more description for this page.",
+            image: Image.asset(
+              "images/1.png",
+              height: 400,
+              width: 400,
+            ),
+          ),
         ],
+        onDone: () {},
+        onSkip: () {},
+        showSkipButton: true,
+        skip: Text(
+          "Skip",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+            color: Color(0xFF6C63FF),
+          ),
+        ),
+        next: Icon(
+          Icons.arrow_forward,
+          color: Color(0xFF6C63FF),
+        ),
+        done: Text(
+          "Done",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 18,
+            color: Color(0xFF6C63FF),
+          ),
+        ),
+        dotsDecorator: DotsDecorator(
+          size: Size.square(10.0),
+          activeSize: Size(20.0, 10.0),
+          color: Colors.black26,
+          activeColor: Color(0xFF6C63FF),
+          spacing: EdgeInsets.symmetric(horizontal: 3.0),
+          activeShape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(25.0),
+          ),
+        ),
       ),
     );
   }
