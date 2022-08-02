8567497e29586ecd3897defc068054d2a5929c64
Francis Rodrigues <francis.rod01@outlook.com>
2022-08-01 22:08:49 -0300

    enh(intro screen): page decoration

--- lib/intro_screen.dart
+++ lib/intro_screen.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 
+const bodyStyle = TextStyle(fontSize: 19.0);
+
+const pageDecoration = PageDecoration(
+  titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
+  bodyTextStyle: bodyStyle,
+  bodyPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+  pageColor: Colors.white,
+  imagePadding: EdgeInsets.zero,
+);
+
 const customDotsDecorator = DotsDecorator(
   size: Size.square(10.0),
   activeSize: Size(20.0, 10.0),
@@ -28,16 +38,11 @@ class IntroScreen extends StatelessWidget {
         scrollPhysics: const BouncingScrollPhysics(),
         pages: [
           PageViewModel(
-            titleWidget: const Text(
-              "Write Title of Page",
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
+            title: "Write Title of Page",
             body:
                 "Write the more description of the page. Write more and more description for this page.",
             image: _buildImage('screen_1.jpg'),
+            decoration: pageDecoration,
           ),
         ],
         onDone: () {
